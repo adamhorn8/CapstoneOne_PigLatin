4cd0f4b15cdbbab40fb3f5e7b7090f3b01cf36b6
Adam Horn <Adam@Adam-Horns-MacBook-Pro.local>
2018-04-27 17:03:08 -0400

    first commit

---
+++ Capstone_One__Pig_Latin/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Capstone_One__Pig_Latin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Welcome to the Pig Latin Translator!");
+
+            while (true)
+            {
+                Console.Write("Enter a word to be translated into Pig Latin: ");
+                string word = Console.ReadLine();
+
+                string vowels = "aeiouAEIOU";
+
+                for (int i = 0; i < word.Length; i++)
+                {
+                    for (int j = 0; j < vowels.Length; j++)
+                    {
+                        if (word[i] == vowels[j])
+                        {
+                            if (i == 0)
+                            {
+                                Console.WriteLine(word + "way");
+                                i = word.Length;
+                                j = vowels.Length;
+                            }
+                            else if(i > 0)
+                            {
+                                string newWord = word.Substring(i, (word.Length - i));
+
+                                string start = word.Substring(0, i);
+
+
+                                Console.WriteLine(newWord + start + "ay");
+
+                                i = word.Length;
+                                j = vowels.Length;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
